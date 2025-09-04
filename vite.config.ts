72ab1c26186771d68d31d44e9a21af4cb8c19ea3
inkcoo <ixiaocang@foxmail.com>
2025-09-04 16:38:28 +0800

    项目同时独立部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -8,7 +9,12 @@ export default defineConfig({
     outDir: 'docs',
     assetsDir: 'assets',
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        extractstamp: resolve(__dirname, 'extractstamp/index.html')
+      },
       output: {
+        entryFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name][extname]'
       }
     }
