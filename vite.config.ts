258a08aeb9b3743052b1f7511ac3c6150362e479
inkcoo <ixiaocang@foxmail.com>
2025-09-04 16:27:18 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // 改为根路径
+  base: '/',
   build: {
-    outDir: "docs"
+    outDir: 'docs',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
   }
 })
 
